211e169ca035191763b71e4816fd3bca0c379406
Rakesh reddy <mallu.rakeshreddy95@gmail.com>
2021-01-15 20:48:12 +0530

    Third commit

--- MaskDet/Solution.py
+++ MaskDet/Solution.py
@@ -2,6 +2,6 @@ class Solution():
     def __init__(self):
         self.a=5
         self.b=20
-    def pri(self,c,d,e):
-        print(self.a+self.b+c+d+e)
+    def pri(self,c,d):
+        print(self.a+self.b+c+d)
 

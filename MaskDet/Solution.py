b6ca347182af8112c7b83370d4a8728aec994734
Rakesh reddy <mallu.rakeshreddy95@gmail.com>
2021-01-15 20:33:31 +0530

    initial commit

---
+++ MaskDet/Solution.py
@@ -0,0 +1,7 @@
+class Solution():
+    def __init__(self):
+        self.a=5
+        self.b=20
+    def pri(self,c):
+        print(self.a+self.b+c)
+
---
+++ MaskDet/__init__.py
@@ -0,0 +1,8 @@
+from Solution import Solution
+
+
+
+
+if __name__=='__main__':
+    solution=Solution()
+    solution.pri(3)
